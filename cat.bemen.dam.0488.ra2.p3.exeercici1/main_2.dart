1d003cf817c2e29892971b36e3ce208268d1b0c2
Bia_Caravaca <mabi24@bemen3.cat>
2025-10-23 20:09:12 +0200

    Añadidos botones con estilos

--- cat.bemen.dam.0488.ra2.p3.exeercici1/main_2.dart
+++ cat.bemen.dam.0488.ra2.p3.exeercici1/main_2.dart
@@ -9,16 +9,53 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Welcome!',  style: TextStyle(fontSize: 32),),
-              SizedBox(height: 20,),
-              Text ('Start learning now', style: TextStyle(fontSize: 16),)
-              ])
+              const Text(
+                'Welcome!',
+                style: TextStyle(fontSize: 32),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Start learning now',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(
+                    const Color.fromARGB(255, 5, 5, 238),
+                  ),
+                ),
+                onPressed: () {
+                  print("Ja! No está programado...");
+                },
+                child: const Text(
+                  "LOGIN",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(
+                    const Color.fromARGB(255, 5, 5, 238),
+                  ),
+                ),
+                onPressed: () {
+                  print("No tienes papeles pal registro primoh..");
+                },
+                child: const Text(
+                  "REGISTER",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
