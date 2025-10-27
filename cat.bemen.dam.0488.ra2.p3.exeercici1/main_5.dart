9fdf0f38a42ce0f90b5c7cd3fb76571a902d77fa
dacaga24-cpu <dacaga24@bemen3.cat>
2025-10-27 17:21:13 +0100

    Añadido ejercicio 5, lista infinita

--- cat.bemen.dam.0488.ra2.p3.exeercici1/main_5.dart
+++ cat.bemen.dam.0488.ra2.p3.exeercici1/main_5.dart
@@ -2,14 +2,41 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
-  
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-   // Lista de nombres
-  static final names = [
+class Message {
+  final String author;
+  final String body;
+
+  Message(this.author, this.body);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Missatges infinits',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      home: const MessageListPage(),
+    );
+  }
+}
+
+class MessageListPage extends StatefulWidget {
+  const MessageListPage({super.key});
+
+  @override
+  State<MessageListPage> createState() => _MessageListPageState();
+}
+
+class _MessageListPageState extends State<MessageListPage> {
+  final List<String> names = [
     "Ellison Curry",
     "Briggs Willis",
     "Alexa Murphy",
@@ -34,16 +61,64 @@ class MainApp extends StatelessWidget {
     "Larry Shepherd",
   ];
 
+  final String body =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac vestibulum nunc.";
+
+  final Random random = Random();
+  final List<Message> messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _generateMoreMessages();
+  }
+
+  void _generateMoreMessages() {
+    // Genera 20 missatges més
+    final newMessages = List.generate(20, (index) {
+      return Message(
+        names[random.nextInt(names.length)],
+        body,
+      );
+    });
+    messages.addAll(newMessages);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Llista de missatges'),
+      ),
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (scrollInfo) {
+          // Si arribem al final, generem més missatges
+          if (scrollInfo.metrics.pixels ==
+              scrollInfo.metrics.maxScrollExtent) {
+            setState(() {
+              _generateMoreMessages();
+            });
+          }
+          return false;
+        },
+        child: ListView.builder(
+          itemCount: messages.length,
+          itemBuilder: (context, index) {
+            final message = messages[index];
+            return ListTile(
+              leading: CircleAvatar(
+                child: Text(message.author[0]),
+              ),
+              title: Text(message.author),
+              subtitle: Text(message.body),
+            );
+          },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {}, // Sense funcionalitat
+        child: const Icon(Icons.add),
       ),
     );
   }
 }
-
