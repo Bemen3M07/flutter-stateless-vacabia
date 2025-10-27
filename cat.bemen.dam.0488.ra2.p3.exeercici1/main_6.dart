5eb96cf0b9b7ddcb7e61a98f38d06c43c6e87fd6
dacaga24-cpu <dacaga24@bemen3.cat>
2025-10-27 18:47:57 +0100

    exercici 6 acabat

---
+++ cat.bemen.dam.0488.ra2.p3.exeercici1/main_6.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class Message {
+  final String author;
+  final String body;
+
+  Message(this.author, this.body);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Missatges infinits',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      home: const MessageListPage(),
+    );
+  }
+}
+
+class MessageListPage extends StatefulWidget {
+  const MessageListPage({super.key});
+
+  @override
+  State<MessageListPage> createState() => _MessageListPageState();
+}
+
+class _MessageListPageState extends State<MessageListPage> {
+  final List<String> names = [
+    "Ellison Curry",
+    "Briggs Willis",
+    "Alexa Murphy",
+    "Cameron Berry",
+    "Annabelle Villarreal",
+    "Nikolai Wiley",
+    "Lauryn Morrow",
+    "Kyree Hardy",
+    "Wells Wilson",
+    "Luna Foster",
+    "Kayden Taylor",
+    "Sofia Mann",
+    "Nehemiah Randall",
+    "Christina Gordon",
+    "Karter Kramer",
+    "Hanna Morales",
+    "Megan Delarosa",
+    "Osiris Johnson",
+    "Emma Atkins",
+    "Cason McKee",
+    "Kori Walls",
+    "Larry Shepherd",
+  ];
+
+  final String body =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac vestibulum nunc.";
+
+  final Random random = Random();
+  final List<Message> messages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _generateMoreMessages();
+  }
+
+  void _generateMoreMessages() {
+    final newMessages = List.generate(20, (index) {
+      return Message(
+        names[random.nextInt(names.length)],
+        body,
+      );
+    });
+    messages.addAll(newMessages);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Llista de missatges'),
+      ),
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (scrollInfo) {
+          if (scrollInfo.metrics.pixels ==
+              scrollInfo.metrics.maxScrollExtent) {
+            setState(() {
+              _generateMoreMessages();
+            });
+          }
+          return false;
+        },
+        child: ListView.builder(
+          itemCount: messages.length,
+          itemBuilder: (context, index) {
+            final message = messages[index];
+            return ListTile(
+              leading: CircleAvatar(
+                child: Text(message.author[0]),
+              ),
+              title: Text(message.author),
+              subtitle: Text(message.body),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {}, // Sense funcionalitat
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Missatges',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contacts),
+            label: 'Contactes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Configuració',
+          ),
+        ],
+        currentIndex: 0, // Sense funcionalitat
+        onTap: (index) {}, // Sense funcionalitat
+      ),
+    );
+  }
+}
